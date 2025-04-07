86ed7584f8aad47e21ed2f247c2fa14efcdacbcb
Tri Sutrisno <mas.trisutrisno@gmail.com>
2025-04-07 07:04:20 +0700

    Add CORS middleware to handle cross-origin requests

--- app.js
+++ app.js
@@ -6,9 +6,17 @@ const app = express();
 const feedRoutes = require('./routes/feed'); 
 
 app.use(express.json()); // Middleware to parse JSON bodies
-app.use(express.urlencoded({ extended: true })); // Middleware to parse URL-encoded bodies
+// app.use(express.urlencoded({ extended: true })); // Middleware to parse URL-encoded bodies
 
 app.use('/feed', feedRoutes); // Use the feed routes for any requests to /feed
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Allow requests from any origin
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Allow specific HTTP methods
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Allow specific headers
+    next(); // Call the next middleware function
+});  
+
 
 app.listen(8080, () => {
     console.log('Server is running on port 8080');
