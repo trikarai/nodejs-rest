a44dae673af56d35062a1174b498cdd3e6e01250
Tri Sutrisno <mas.trisutrisno@gmail.com>
2025-04-07 09:52:48 +0700

    Update feedController to include detailed post structure and add example image

--- controllers/feedController.js
+++ controllers/feedController.js
@@ -1,21 +1,27 @@
 exports.getPosts = (req, res, next) => {
     res.status(200).json({
-        posts: [
-            {
-                id: 'p1',
-                title: 'First Post',
-                content: 'This is the first post!',
-            },
-            {
-                id: 'p2',
-                title: 'Second Post',
-                content: 'This is the second post!',
-            },
-        ],
+      posts: [
+        {
+          _id: "p1",
+          title: "First Post",
+          content: "This is the first post!",
+          imageUrl: "/images/1743836947636-images.png", // Example image URL
+          creator: { name: "Tri" }, // Example creator object
+          createdAt: new Date(), // Example creation date
+        },
+        {
+          _id: "p2",
+          title: "Second Post",
+          content: "This is the second post!",
+          imageUrl: "/images/1743836947636-images.png",
+          creator: { name: "Tri" }, // Example creator object
+          createdAt: new Date(), // Example creation date
+        },
+      ],
     });
 }
 
-exports.createPost = (req, res, next) => {
+exports.createPost = (req, res) => {
     const title = req.body.title; // Extract title from request body
     const content = req.body.content; // Extract content from request body
 
