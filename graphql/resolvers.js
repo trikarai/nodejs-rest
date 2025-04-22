640e50689c566472a14651414eda777f252fa1cd
Tri Sutrisno <mas.trisutrisno@gmail.com>
2025-04-23 06:13:54 +0700

    Implement user creation resolver and update GraphQL schema for user management

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,11 +1,41 @@
+const bcrypt = require("bcryptjs"); // Import bcrypt for password hashing
+const User = require('../models/user'); // Import the User model to interact with the database
+
 // This file contains the resolvers for the GraphQL API.
 // It defines the structure of the API and how to resolve queries and mutations.
-const rootValue =  {
-    hello: () => {
-        return {
-            text: "Hello World!",
-            views: 1234,
-        };
-    },
-}
+const rootValue = {
+  createUser: async (args, req) => {
+    try {
+      // Check if a user with the same email already exists
+      const existingUser = await User.findOne({ email: args.userInput.email });
+      if (existingUser) {
+        const error = new Error("User already exists!"); // Create an error if the user already exists
+        error.code = 422; // Set the error code to 422 (Unprocessable Entity)
+        throw error; // Throw the error to be caught by the GraphQL handler
+      }
+
+      // Hash the password
+      const hashedPassword = await bcrypt.hash(args.userInput.password, 12);
+
+      // Create a new user object
+      const user = new User({
+        email: args.userInput.email,
+        name: args.userInput.name,
+        password: hashedPassword,
+      });
+
+      // Save the user to the database
+      const createdUser = await user.save();
+
+      // Return the created user object
+      return { ...createdUser._doc, _id: createdUser._id.toString() };
+    } catch (err) {
+      console.log(err); // Log any errors that occur during the process
+      throw err; // Throw the error to be caught by the GraphQL handler
+    }
+  },
+};
+
+// The rootValue object contains the resolvers for the GraphQL API
+// Each key in the object corresponds to a field in the GraphQL schema
 module.exports = { rootValue };
--- graphql/schema.js
+++ graphql/schema.js
@@ -1,17 +1,42 @@
 const { buildSchema } = require('graphql');
 
 const schema = buildSchema(`
-    type TestData {
-        text: String!
-        views: Int!
+    type Post {
+        _id: ID!
+        title: String!
+        content: String!
+        imageUrl: String!
+        creator: User!
+        createdAt: String!
+        updatedAt: String!
+    }
+
+    type User {
+        _id: ID!
+        name: String!
+        email: String!
+        password: String!
+        status: String!
+        posts: [Post!]!
+    }
+    
+    input UserInput {
+        name: String!
+        email: String!
+        password: String!
+    }
+
+    type RootMutation {
+        createUser(userInput : UserInput): User!
     }
 
     type RootQuery {
-        hello: TestData!
-    } 
+        users: [User!]!
+    }
 
     schema {
         query: RootQuery
+        mutation: RootMutation
   }
 `);
 
