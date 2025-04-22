95aaf2daeaa89060c1efe38486bec7d6b0f79357
Tri Sutrisno <mas.trisutrisno@gmail.com>
2025-04-22 23:04:13 +0700

    Refactor app.js to integrate GraphQL handler and remove unused routes

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 require("dotenv").config();
-const http = require("http");
 const path = require("path");
 
 const express = require("express");
@@ -7,6 +6,11 @@ const mongoose = require("mongoose"); // Import mongoose for MongoDB connection
 const multer = require("multer"); // Import multer for file uploads
 const { v4: uuidv4 } = require("uuid"); // Import uuid for generating unique IDs
 
+const { schema } = require("./graphql/schema"); // Import GraphQL schema
+const { rootValue } = require("./graphql/resolvers"); // Import GraphQL resolvers
+const { createHandler } = require('graphql-http/lib/use/express');
+
+ 
 const app = express();
 
 const fileStorage = multer.diskStorage({
@@ -32,17 +36,10 @@ const fileFilter = (req, file, cb) => {
 
 const fileUpload = multer({ storage: fileStorage, fileFilter: fileFilter }); // Initialize multer with the storage configuration
 
-// Import the routes
-const feedRoutes = require("./routes/feed");
-const authRoutes = require("./routes/auth");
-
 app.use(express.json()); // Middleware to parse JSON bodies
 // app.use(express.urlencoded({ extended: true })); // Middleware to parse URL-encoded bodies
 app.use(fileUpload.single("file")); // Middleware to handle single file uploads with the field name 'file'
 app.use("/images", express.static(path.join(__dirname, "images"))); // Serve static files from the 'images' directory
-
-app.use("/feed", feedRoutes); // Use the feed routes for any requests to /feed
-app.use("/auth", authRoutes); // Use the feed routes for any requests to /feed
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*"); // Allow requests from any origin
@@ -51,6 +48,24 @@ app.use((req, res, next) => {
   next(); // Call the next middleware function
 });
 
+app.use(
+  "/graphql", (req, res, next) =>  
+  createHandler({
+    schema: schema,
+    rootValue: rootValue,
+    formatError: (err) => {
+      if (err.originalError) {
+        const message = err.originalError.message || "An error occurred."; // Get the error message
+        const code = err.originalError.code || 500; // Get the error code or default to 500
+        return { message: message, status: code }; // Return the error response
+      }
+      return err; // Return the original error if no specific error is found
+    },
+    graphiql: true, // Enable GraphiQL interface for testing queries
+  })(req, res, next) // Use the GraphQL handler for incoming requests
+  // Pass the request, response, and next function to the handler 
+);
+
 app.use((error, req, res, next) => {
   const status = error.statusCode || 500; // Get the status code from the error or default to 500
   const message = error.message; // Get the error message
@@ -58,25 +73,12 @@ app.use((error, req, res, next) => {
   res.status(status).json({ message: message, data: data }); // Send the error response as JSON
 });
 
-const server = http.createServer(app);
-
 mongoose
     .connect(process.env.MONGODB_URI)
     .then(() => {
-      server.listen(8080, () => {
+      app.listen(8080, () => {
         console.log("Server is running on port 8080");
       });
       console.log("Connected to MongoDB");
-
-      const io = require("./socket").init(server); // Initialize socket.io with the server
-      
-      io.on("connection", (socket) => {
-        console.log("Client connected:", socket.id); // Log when a client connects
-        socket.on("disconnect", () => {
-          console.log("Client disconnected");
-        });
-      });
     })
     .catch((err) => console.log(err));
-
- 
