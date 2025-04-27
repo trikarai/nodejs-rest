f130d80e598627a328c4d79a496c0a751d818248
Tri Sutrisno <mas.trisutrisno@gmail.com>
2025-04-27 20:33:27 +0700

    Add getUserStatus method to retrieve user status and handle errors

--- controllers/authController.js
+++ controllers/authController.js
@@ -93,3 +93,20 @@ exports.signup = async (req, res) => {
             next(err); // Pass the error to the next middleware
         });
 }
+
+exports.getUserStatus = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.userId);
+    if (!user) {
+      const error = new Error('User not found.');
+      error.statusCode = 404;
+      throw error;
+    }
+    res.status(200).json({ status: user.status });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
+};
